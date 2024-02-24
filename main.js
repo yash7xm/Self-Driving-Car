452dcd421c013bf7c76e5da2e4e6b6cfc658e245
yash7xm <yash.7xm@gmail.com>
2024-02-24 12:34:52 +0530

    basic car added

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 canvas.height = window.innerHeight;
 
+const car = new Car(100,100,30,50);
+
 const ctx = canvas.getContext("2d");
+
+animate();
+
+function animate() {
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
