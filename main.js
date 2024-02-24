a41c418a9c3d1400595713bfaa623cbbb28c7fe9
yash7xm <yash.7xm@gmail.com>
2024-02-24 12:53:46 +0530

    car movements added

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
-canvas.height = window.innerHeight;
 
 const car = new Car(100, 100, 30, 50);
 
@@ -9,6 +8,10 @@ const ctx = canvas.getContext("2d");
 animate();
 
 function animate() {
+  car.update();
+
+  canvas.height = window.innerHeight;
+
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
