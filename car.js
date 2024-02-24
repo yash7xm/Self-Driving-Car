1477207841e9b47a135b440f8d53417724e222a9
yash7xm <yash.7xm@gmail.com>
2024-02-24 19:10:35 +0530

    sensor rays added

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -70,5 +72,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
