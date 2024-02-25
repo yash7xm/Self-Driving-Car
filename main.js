92f8071c84a9e9e0d00a7d766a68b0663ea5275a
yash7xm <yash.7xm@gmail.com>
2024-02-25 14:37:17 +0530

    added brain in cars

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 const ctx = canvas.getContext("2d");
