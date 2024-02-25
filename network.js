00c39c65f270f080a3bc55a389ed13fd92f336e3
yash7xm <yash.7xm@gmail.com>
2024-02-25 14:08:50 +0530

    neural networks setup

---
+++ network.js
@@ -0,0 +1,26 @@
+class Level1 {
+  constructor(inputCount, outputCount) {
+    this.inputs = new Array(inputCount);
+    this.outputs = new Array(outputCount);
+    this.biases = new Array(outputCount);
+
+    this.weights = [];
+    for (let i = 0; i < inputCount; i++) {
+      this.weights[i] = new Array(outputCount);
+    }
+
+    Level1.#randomize(this);
+  }
+
+  static #randomize(level) {
+    for (let i = 0; i, level.length; i++) {
+      for (let j = 0; j < level.outputs.length; j++) {
+        level.weights[i][j] = Math.random() * 2 - 1;
+      }
+    }
+
+    for (let i = 0; i < level.biases.length; i++) {
+      level.biases[i] = Math.random() * 2 - 1;
+    }
+  }
+}
