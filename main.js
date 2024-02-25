579ff4ee64d0e5f67579cdfab7549f2ad03eb35c
yash7xm <yash.7xm@gmail.com>
2024-02-25 13:14:58 +0530

    traffic generated

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 const ctx = canvas.getContext("2d");
 
 animate();
 
 function animate() {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders);
+  }
   car.update(road.borders);
 
   canvas.height = window.innerHeight;
@@ -17,6 +21,9 @@ function animate() {
   ctx.translate(0, -car.y + canvas.height * 0.7);
 
   road.draw(ctx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx);
+  }
   car.draw(ctx);
 
   ctx.restore();
