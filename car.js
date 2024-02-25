5dbbf7cae50af5b85bf37cdad4613878169a5af8
yash7xm <yash.7xm@gmail.com>
2024-02-25 12:59:04 +0530

    collision added

--- car.js
+++ car.js
@@ -10,6 +10,7 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+    this.damage = false;
 
     this.sensor = new Sensor(this);
     this.controls = new Controls();
@@ -18,7 +19,16 @@ class Car {
   update(roadBorders) {
     this.#move();
     this.polygon = this.#createPolygon();
+    this.damaged = this.#assessDamage(roadBorders);
     this.sensor.update(roadBorders);
+  }
+
+  #assessDamage(roadBorders) {
+    for(let i=0; i<roadBorders.length; i++){
+      if(polysIntersect(this.polygon, roadBorders[i])) {
+        return true;
+      }
+    }
   }
 
   #createPolygon() {
@@ -88,6 +98,11 @@ class Car {
   }
 
   draw(ctx) {
+    if(this.damaged) {
+      ctx.fillStyle = "grey";
+    } else {
+      ctx.fillStyle = "black";
+    }
     ctx.beginPath();
     ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
     for (let i = 0; i < this.polygon.length; i++) {
