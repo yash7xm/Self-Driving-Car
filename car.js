3c9b25857afeccef6696bec4c8c56e578d69da09
yash7xm <yash.7xm@gmail.com>
2024-02-25 13:28:56 +0530

    traffic sensors detection

--- car.js
+++ car.js
@@ -109,11 +109,11 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx) {
+  draw(ctx, color) {
     if (this.damaged) {
       ctx.fillStyle = "grey";
     } else {
-      ctx.fillStyle = "black";
+      ctx.fillStyle = color;
     }
     ctx.beginPath();
     ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
