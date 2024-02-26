cb60692ba4049ff9cf58396fb1fed103e1740174
yash7xm <yash.7xm@gmail.com>
2024-02-26 17:44:42 +0530

    added more examples

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
-const N = 100;
+const N = 1;
 const cars = generateCars(N);
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
@@ -18,7 +18,7 @@ if (localStorage.getItem("bestBrain")) {
       localStorage.getItem("bestBrain")
     )
     if(i!=0) {
-      NeuralNetwork.mutate(cars[i].brain,0.2);
+      NeuralNetwork.mutate(cars[i].brain,0.1);
     }
   }
   bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
@@ -27,7 +27,16 @@ if (localStorage.getItem("bestBrain")) {
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
   new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2)
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -900, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -1000, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -1200, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -1400, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -1600, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -1800, 30, 50, "DUMMY", 2)
 ];
 
 animate();
