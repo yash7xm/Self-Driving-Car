1971f653f813e55ca2cec822ab1e6c36c7ec49e7
yash7xm <yash.7xm@gmail.com>
2024-02-26 17:03:22 +0530

    network mutate added

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ const N = 100;
 const cars = generateCars(N);
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
+  for(let i=0;i<cars.length; i++){
+    cars[i].brain = JSON.parse(
+      localStorage.getItem("bestBrain")
+    )
+    if(i!=0) {
+      NeuralNetwork.mutate(cars[i].brain,0.2);
+    }
+  }
   bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
 }
 
